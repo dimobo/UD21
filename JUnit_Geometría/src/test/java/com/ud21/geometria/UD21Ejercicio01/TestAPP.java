bbe333923943b9243bd3981018a8ea16390e6e7c
Dídac Montero Borràs <37572665+dimobo@users.noreply.github.com>
2021-08-09 10:01:51 +0200

    update junit

---
+++ JUnit_Geometría/src/test/java/com/ud21/geometria/UD21Ejercicio01/TestAPP.java
@@ -0,0 +1,14 @@
+package com.ud21.geometria.UD21Ejercicio01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestAPP {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

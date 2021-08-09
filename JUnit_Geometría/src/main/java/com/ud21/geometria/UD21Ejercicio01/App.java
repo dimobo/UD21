2c912b0e3fc572e15ec4ec0b1159c9dc4e0a6316
Dídac Montero Borràs <37572665+dimobo@users.noreply.github.com>
2021-08-09 09:46:29 +0200

    geometría initial push

---
+++ JUnit_Geometría/src/main/java/com/ud21/geometria/UD21Ejercicio01/App.java
@@ -0,0 +1,13 @@
+package com.ud21.geometria.UD21Ejercicio01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ JUnit_Geometría/src/test/java/com/ud21/geometria/UD21Ejercicio01/AppTest.java
@@ -0,0 +1,20 @@
+package com.ud21.geometria.UD21Ejercicio01;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
